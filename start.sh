9204aa0fadf3aed3bb80eb8f53ca4c42911dbd5a
Nick Gregory <github@openenterprise.co.uk>
2018-09-17 20:31:11 +0000

    add monitoring

--- start.sh
+++ start.sh
@@ -2,4 +2,10 @@
 if [ ! -e /dev/spidev1.0 ]; then
   config-pin overlay BB-SPIDEV0
 fi
+
+/etc/node_exporter --web.listen-address "127.0.0.1:81" &
+
+export PATH=$PATH:/opt/gwexporter/bin
+node /opt/gwexporter/gwexporter.js &
+
 python /opt/ttn-gateway/run.py
